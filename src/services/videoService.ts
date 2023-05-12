e44793df0b6a810d06e09051839b38973814a052
hxsggsz <vt.hugo.2021@gmail.com>
2023-05-12 02:54:49 -0300

    fix supabase connection with env

--- src/services/videoService.ts
+++ src/services/videoService.ts
@@ -1,8 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
-const PROJECT_URL = process.env.PROJECT_URL
-const PUBLIC_KEY = process.env.PUBLIC_KEY
-export const supabase = createClient(PROJECT_URL!, PUBLIC_KEY!);
+const PROJECT_URL = "https://sdrsduyvpgydxlxzswzl.supabase.co"
+let PUBLIC_KEY
+if(process.env.PUBLIC_KEY) {
+  PUBLIC_KEY = process.env.PUBLIC_KEY
+}
+
+export const supabase = createClient(PROJECT_URL, PUBLIC_KEY);
 
 export default function videoService() {
   return {
