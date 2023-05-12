4eb85ef5a0a404c3632c09c8705638b78549bb24
hxsggsz <vt.hugo.2021@gmail.com>
2023-05-12 02:38:44 -0300

    add enviroment variables to connect to supabase

--- src/services/videoService.ts
+++ src/services/videoService.ts
@@ -1,9 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const PROJECT_URL = "https://sdrsduyvpgydxlxzswzl.supabase.co";
-const PUBLIC_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkcnNkdXl2cGd5ZHhseHpzd3psIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxOTQwNDYsImV4cCI6MTk4Mzc3MDA0Nn0.7k99uEvsLo5lFM09_v6n2Ay_aNSgE6qncexv6wyw8-U";
-export const supabase = createClient(PROJECT_URL, PUBLIC_KEY);
+const PROJECT_URL = process.env.PROJECT_URL
+const PUBLIC_KEY = process.env.PUBLIC_KEY
+export const supabase = createClient(PROJECT_URL!, PUBLIC_KEY!);
 
 export default function videoService() {
   return {
