388d1dcffca1a5c06f37ed1d2d3ad35f5a3eca8d
hxsggsz <vt.hugo.2021@gmail.com>
2023-05-12 13:54:56 -0300

    fix connection with supabase error

--- src/services/videoService.ts
+++ src/services/videoService.ts
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 const PROJECT_URL = process.env.NEXT_PUBLIC_SUPABASE_URL
 const PUBLIC_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-export const supabase = createClient(PROJECT_URL, PUBLIC_KEY);
+export const supabase = createClient(PROJECT_URL!, PUBLIC_KEY!);
 
 export default function videoService() {
   return {
